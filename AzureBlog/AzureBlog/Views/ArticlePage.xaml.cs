922b599acc935aa0d4c8d7c6f4e6775973572322
Simon Brown <simon.j.brown@outlook.com>
2016-06-03 16:33:50 +1000

    Basic drill-in without formatting

---
+++ AzureBlog/AzureBlog/Views/ArticlePage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using AzureBlog.Models;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AzureBlog.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ArticlePage : Page
+    {
+        public ArticlePage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is string)
+            {
+                this.Message = String.Format("Clicked on {0}", e.Parameter);
+            }
+            else if (e.Parameter is IArticle)
+            {
+                IArticle Article = (IArticle) e.Parameter;
+                this.Message = Article.Content;
+            }
+            else
+            {
+                
+                this.Message = "There was a problem rendering this article";
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        public string Message { get; set; }
+
+    }
+}
--- AzureBlog/AzureBlog/Views/NewspaperPage.xaml.cs
+++ AzureBlog/AzureBlog/Views/NewspaperPage.xaml.cs
@@ -32,6 +32,26 @@ namespace AzureBlog.Views
 
         }
 
+        private void NewspaperGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            this.Frame.Navigate(
+                typeof(ArticlePage),
+                e.ClickedItem,
+                new Windows.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+        }
+
+
+
+        //      private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        //      {
+        //          this.Frame.Navigate(
+        //              typeof(BasicSubPage),
+        //              e.ClickedItem,
+        //              new Windows.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+        //      }
+
+
+
 
     }
 }
