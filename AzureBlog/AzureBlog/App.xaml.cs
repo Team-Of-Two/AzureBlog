dd1fe0a0848e2c6280ae71a575f91c0afa75962b
Simon Brown <simon.j.brown@outlook.com>
2016-06-03 13:48:53 +1000

    Comment to ensure Sourcecontrol is working

--- AzureBlog/AzureBlog/App.xaml.cs
+++ AzureBlog/AzureBlog/App.xaml.cs
@@ -28,6 +28,7 @@ namespace AzureBlog
     sealed partial class App : Application
     {
         Controllers.RSSNewspaperController _currentNewspaperController = new Controllers.RSSNewspaperController("https://azure.microsoft.com/en-us/blog/feed/");
+        //need to create singleton at this level. 
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
