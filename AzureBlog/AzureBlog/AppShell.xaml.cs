35e6dab6960792db3d4cd3bc1b574e9d1993e7a2
Simon <Simon@SIMON-M4300>
2016-06-13 00:45:42 +1000

    About screen added. Hero colour (Azure) added to Nav Bar hamburger. Links to URLs should always open in the external browser now.

--- AzureBlog/AzureBlog/AppShell.xaml.cs
+++ AzureBlog/AzureBlog/AppShell.xaml.cs
@@ -61,6 +61,13 @@ namespace AzureBlog
                     Symbol = Symbol.Read,
                     Label = "News Items",
                     DestPage = typeof(NewspaperPage)
+                },
+
+                new NavMenuItem()
+                {
+                    Symbol = Symbol.ContactInfo,
+                    Label = "About",
+                    DestPage = typeof(About)
                 }
 
             });
--- AzureBlog/AzureBlog/Views/ArticlePage.xaml.cs
+++ AzureBlog/AzureBlog/Views/ArticlePage.xaml.cs
@@ -55,5 +55,17 @@ namespace AzureBlog.Views
 
         public string Message { get; set; }
 
+        private async void ArticleWebview_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            //if there is a hyperlink in the page being displayed, ensure a valid URI always opens in the 
+            //default system launcher. 
+            //if (null != args.Uri && args.Uri.OriginalString == "URL OF INTEREST")
+            if (null != args.Uri)
+            {
+                args.Cancel = true;
+                await Windows.System.Launcher.LaunchUriAsync(args.Uri);
+            }
+
+        }
     }
 }
