037d89c924bcaf2204e8400ccb85027406961517
Simon Brown <simon.j.brown@outlook.com>
2016-07-19 17:13:26 +1000

    Updated properties. Updated icons. Changed about page to acknowledge icon author.

--- WindowsFormsApplication1/WindowsFormsApplication1/Properties/AssemblyInfo.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Properties/AssemblyInfo.cs
@@ -5,14 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Azure News Reader")]
+[assembly: AssemblyTitle("WindowsFormsApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TeamOfTwo")]
-[assembly: AssemblyProduct("Azure News Reader")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0274bd72-cc79-45c8-8c3f-92a9b153d8e2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
