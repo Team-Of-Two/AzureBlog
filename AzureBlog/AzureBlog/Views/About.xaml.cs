c31d9b46189e3049d5e8b1df0a69ed04a3552061
Simon Brown <simon.j.brown@outlook.com>
2016-07-20 12:12:21 +1000

    Icons update. Mail us button added to about.

--- AzureBlog/AzureBlog/Views/About.xaml.cs
+++ AzureBlog/AzureBlog/Views/About.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
+using Windows.ApplicationModel.Email;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -31,5 +32,25 @@ namespace AzureBlog.Views
                 Package.Current.Id.Version.Minor,
                 Package.Current.Id.Version.Build));
         }
+
+        private async void btnMailUs_Click(object sender, RoutedEventArgs e)
+        {
+            EmailMessage emailMessage = new EmailMessage();
+            emailMessage.To.Add(new EmailRecipient("TeamOfTwo@outlook.com.au"));
+            //string messageBody = "Hello World";
+            //emailMessage.Body = messageBody;
+            //StorageFolder MyFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            //StorageFile attachmentFile = await MyFolder.GetFileAsync("MyTestFile.txt");
+            //if (attachmentFile != null)
+            //{
+            //    var stream = Windows.Storage.Streams.RandomAccessStreamReference.CreateFromFile(attachmentFile);
+            //    var attachment = new Windows.ApplicationModel.Email.EmailAttachment(
+            //             attachmentFile.Name,
+            //             stream);
+            //    emailMessage.Attachments.Add(attachment);
+            //}
+            await EmailManager.ShowComposeNewEmailAsync(emailMessage);
+        }
     }
+    
 }
